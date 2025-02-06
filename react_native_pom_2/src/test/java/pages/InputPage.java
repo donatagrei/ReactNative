c4ee613b367cdcd97b4dfef10c8100634b280483
Donata <donata.greiciute97@gmail.com>
2025-02-06 13:49:26 +0200

    Update InputPage.java

--- react_native_pom_2/src/test/java/pages/InputPage.java
+++ react_native_pom_2/src/test/java/pages/InputPage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 
 public class InputPage extends BasePage { //extends PageBase class, inheriting its properties and methods.
 
-    //locates an Android UI element using uiAutomator strategy
     @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Input\")")
     WebElement inputBlock;
 
@@ -20,19 +19,16 @@ public class InputPage extends BasePage { //extends PageBase class, inheriting i
     @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Colored Placeholder\")")
     WebElement inputWithColoredPlaceholder;
 
-    // Constructor is essential for proper initialization of the AppiumDriver within
-    // both the InputPage and its superclass PageBase
+    
     public InputPage(AppiumDriver appiumDriver) {
         super(appiumDriver);
     }
 
-    // Method which waits for element and then clicks that element
     public void clickInputBlock() {
         waitForVisibility(inputBlock);
         inputBlock.click();
     }
 
-    // Returns text of the element
     public String checkTextInputWithLabelTitle() {
         return textInputWithLabelTitle.getText();
     }
