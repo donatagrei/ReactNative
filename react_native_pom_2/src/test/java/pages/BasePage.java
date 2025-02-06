7e251da298d5c4a7ac43fdefd399b6c0dc008c82
Donata <donata.greiciute97@gmail.com>
2025-02-06 13:53:54 +0200

    Update BasePage.java

--- react_native_pom_2/src/test/java/pages/BasePage.java
+++ react_native_pom_2/src/test/java/pages/BasePage.java
@@ -20,18 +20,18 @@ public class BasePage {
         PageFactory.initElements(new AppiumFieldDecorator(appiumDriver), this);
     }
 
-    // reusable method for different WebElements
+    // Reusable method for different WebElements
     public void waitForVisibility(WebElement element){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOf(element));
     }
 
-    // reusable method for different menu blocks
+    // Reusable method for different menu blocks
     public void clickBlock(String block){
         driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"" + block + "\")")).click();
     }
 
-    // reusable method for scrolling until the wanted text becomes visible
+    // Reusable method for scrolling until the wanted text becomes visible
     public void scrollToText(String text){
         driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector().className(\"android.widget.ScrollView\")).scrollTextIntoView(\"" + text + "\")"));
     }
